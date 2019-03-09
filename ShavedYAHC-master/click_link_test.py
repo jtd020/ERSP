d4645d907d8ed01ebc32eeb21aca731f6c1bcb38
jtd020 <30811361+jtd020@users.noreply.github.com>
2019-03-08 16:56:16 -0800

    Implemented new approach with tabs

--- ShavedYAHC-master/click_link_test.py
+++ ShavedYAHC-master/click_link_test.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 from PIL import Image
 from io import BytesIO
 import cv2
@@ -20,7 +22,6 @@ def main(url, image_location, headless):
     driver = webdriver.Firefox(options=options)
     driver.get(url)
     elements = driver.find_elements_by_tag_name("a")
-    print(len(elements))
     count = 0
     for element in elements:
         count += 1
@@ -31,11 +32,25 @@ def main(url, image_location, headless):
 
         try:
             before = take_screenshot(driver.get_screenshot_as_png(), location, size, "before" + str(count), image_location)
-            print("finished taking 'before' screenshot")
-            el.click()
+            # print("finished taking 'before' screenshot")
+            # el.click()
+            # driver.implicitly_wait(15)
+            # driver.get(url)
+            # print("finsihed going back")
+            ActionChains(driver).key_down(Keys.CONTROL).click(el).key_up(Keys.CONTROL).perform()
+            driver.implicitly_wait(10)
+            driver.delete_all_cookies()
+            driver.implicitly_wait(10)
+            driver.refresh()
+            driver.implicitly_wait(10)
+            original_handle = driver.window_handles[0]
+            for handle in driver.window_handles:
+                if handle != original_handle:
+                    driver.switch_to.window(handle)
+                    driver.close()
             driver.implicitly_wait(15)
-            driver.get(url)
-            print("finsihed going back")
+            driver.switch_to.window(driver.window_handles[0])
+            # print("finsihed going back")
         except:
             continue
 
@@ -44,8 +59,13 @@ def main(url, image_location, headless):
 
         if compare_images(before, after):
             break
+        # os.remove(before)
+        # os.remove(after)
 
     driver.close()
+    # for handle in driver.window_handles:
+    #     driver.switch_to.window(handle)
+    #     driver.close()
 
 
 def take_screenshot(image, location, size, name, image_location):
@@ -59,6 +79,9 @@ def take_screenshot(image, location, size, name, image_location):
 
     im.save(image_location + name + ".png")
     return str(image_location + name + ".png")
+    # with BytesIO() as f:
+    #     im.save(f, format='PNG')
+    #     return f.getvalue()
 
 
 def compare_images(before, after):
@@ -72,10 +95,21 @@ def compare_images(before, after):
         return False
     else:
         cv2.imwrite("result.png", difference)
+        cv2.imwrite("before.png", before_image)
+        cv2.imwrite("after.png", after_image)
         print("images are different")
         print(after)
         return True
 
 
+def element_href_test(url):
+    driver = webdriver.Firefox()
+    driver.get(url)
+    elements = driver.find_elements_by_tag_name("a")
+    for element in elements:
+        print (element.get_attribute('href'))
+
+
 if __name__ == "__main__":
-    main("https://www.facebook.com/", "images_facebook\\", True)
+    main("https://www.youtube.com", "images_youtube\\", False)
+    # element_href_test("https://www.youtube.com")
