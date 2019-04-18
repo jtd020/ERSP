244dca834e3c76c02acd7f5ba951f2fc0d686f88
jtd020 <30811361+jtd020@users.noreply.github.com>
2019-04-18 12:45:25 -0700

    Updated code to include improved link clicking.

--- ShavedYAHC-master/click_link_test.py
+++ ShavedYAHC-master/click_link_test.py
@@ -2,12 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from PIL import Image
 from io import BytesIO
+
 import cv2
 import numpy as np
 import os
 import shutil
+import time
 
 
 def main(url, image_location, headless):
@@ -20,52 +24,88 @@ def main(url, image_location, headless):
         options.headless = True
 
     driver = webdriver.Firefox(options=options)
+    wait = WebDriverWait(driver, 3)
     driver.get(url)
+    time.sleep(3)
+    driver.maximize_window()
+
+    # changes all links open in new tabs
+    driver.execute_script("""
+        var items = document.getElementsByTagName("a");
+        Array.from(items).forEach(function(item){
+        var attributes = Object.values(item.attributes);
+        for (i=0; i < attributes.length; i++) {
+            console.log(typeof attributes[i].nodeName.toString());
+            if (attributes[i].nodeName.toString() != "href" && attributes[i].nodeName.toString() != "class") {
+                item.removeAttribute(attributes[i].nodeName.toString());
+            }
+        }
+        item.setAttribute("target", "_blank");
+        var new_element = item.cloneNode(true);
+        item.parentNode.replaceChild(new_element, item); 
+        item.addEventListener("click", function(event){
+            event.stopPropagation();
+            event.preventDefault();
+            return false;
+        });
+    });""")
+
     elements = driver.find_elements_by_tag_name("a")
     count = 0
+
     for element in elements:
         count += 1
-        re_elements = driver.find_elements_by_tag_name("a")
-        el = re_elements[elements.index(element)]
-        location = el.location
-        size = el.size
 
         try:
+            el = element
+            location = el.location
+            size = el.size
+            ActionChains(driver).move_to_element(el).perform()
             before = take_screenshot(driver.get_screenshot_as_png(), location, size, "before" + str(count), image_location)
-            # print("finished taking 'before' screenshot")
-            # el.click()
-            # driver.implicitly_wait(15)
-            # driver.get(url)
-            # print("finsihed going back")
-            ActionChains(driver).key_down(Keys.CONTROL).click(el).key_up(Keys.CONTROL).perform()
+            wait.until(EC.visibility_of(el))
+
+            # clicks on link
+            ActionChains(driver).key_down(Keys.CONTROL).click(el).key_up(Keys.CONTROL).send_keys(Keys.ESCAPE).perform()
+            time.sleep(1)
+            driver.switch_to.window(driver.window_handles[0])
             driver.implicitly_wait(10)
             driver.delete_all_cookies()
             driver.implicitly_wait(10)
-            driver.refresh()
-            driver.implicitly_wait(10)
+
             original_handle = driver.window_handles[0]
+            # deletes all uncessary tabs
             for handle in driver.window_handles:
                 if handle != original_handle:
                     driver.switch_to.window(handle)
                     driver.close()
+
+            ActionChains(driver).move_to_element(el)
+            driver.switch_to.window(driver.window_handles[0])
+
+        except:
+            original_handle = driver.window_handles[0]
+
+            for handle in driver.window_handles:
+                if handle != original_handle:
+                    driver.switch_to.window(handle)
+                    driver.close()
+
             driver.implicitly_wait(15)
             driver.switch_to.window(driver.window_handles[0])
-            # print("finsihed going back")
-        except:
             continue
 
         after = take_screenshot(driver.get_screenshot_as_png(), location, size, "after" + str(count), image_location)
-        print("finished taking 'after' screenshot")
 
         if compare_images(before, after):
+            print("Feature detected")
             break
-        # os.remove(before)
-        # os.remove(after)
+        os.remove(before)
+        os.remove(after)
 
-    driver.close()
-    # for handle in driver.window_handles:
-    #     driver.switch_to.window(handle)
-    #     driver.close()
+    shutil.rmtree(image_location)
+    for handle in driver.window_handles:
+        driver.switch_to.window(handle)
+        driver.close()
 
 
 def take_screenshot(image, location, size, name, image_location):
@@ -79,9 +119,6 @@ def take_screenshot(image, location, size, name, image_location):
 
     im.save(image_location + name + ".png")
     return str(image_location + name + ".png")
-    # with BytesIO() as f:
-    #     im.save(f, format='PNG')
-    #     return f.getvalue()
 
 
 def compare_images(before, after):
@@ -91,25 +128,13 @@ def compare_images(before, after):
     result = not np.any(difference)
 
     if result:
-        print("images are the same")
         return False
     else:
         cv2.imwrite("result.png", difference)
         cv2.imwrite("before.png", before_image)
         cv2.imwrite("after.png", after_image)
-        print("images are different")
-        print(after)
         return True
 
 
-def element_href_test(url):
-    driver = webdriver.Firefox()
-    driver.get(url)
-    elements = driver.find_elements_by_tag_name("a")
-    for element in elements:
-        print (element.get_attribute('href'))
-
-
 if __name__ == "__main__":
-    main("https://www.youtube.com", "images_youtube\\", False)
-    # element_href_test("https://www.youtube.com")
+    main("https://www.facebook.com", "images_facebook\\", False)
